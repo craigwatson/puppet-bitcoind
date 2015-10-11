2c6c3241f8165311f3a176aac73050093a426cfd
Craig Watson <craig@cwatson.org>
2015-10-11 12:02:01 +0100

    Require apt update instead of PPA

--- manifests/install.pp
+++ manifests/install.pp
@@ -19,19 +19,17 @@
 #
 class bitcoind::install {
 
-  apt::ppa { 'ppa:bitcoin/bitcoin':
-    notify => Exec['apt_update'],
-  }
+  apt::ppa { 'ppa:bitcoin/bitcoin': }
 
   package { 'bitcoind':
     ensure  => present,
-    require => Apt::Ppa['ppa:bitcoin/bitcoin'],
+    require => Exec['apt_update'],
   }
 
   if $bitcoind::install_gui == true {
     package { 'bitcoin-qt':
       ensure  => present,
-      require => Apt::Ppa['ppa:bitcoin/bitcoin'],
+      require => Exec['apt_update'],
     }
   }
 
