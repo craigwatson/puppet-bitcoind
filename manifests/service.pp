066fd2e332d3dd2748dd762b425fbd1ef31fcd61
Craig Watson <craig@cwatson.org>
2016-06-20 20:14:43 +0100

    Fixing service dependency

--- manifests/service.pp
+++ manifests/service.pp
@@ -20,6 +20,6 @@ class bitcoind::service {
   service { 'bitcoind':
     ensure  => $::bitcoind::service_ensure,
     enable  => $::bitcoind::service_enable,
-    require => File['/etc/init/bitcoind.conf'],
+    require => File[ $::bitcoind::params::init_path],
   }
 }
