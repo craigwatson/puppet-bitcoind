5c7aa6d4dd21381f1af431e0e434bfa8eaab1dcd
Craig Watson <craig@bashton.com>
2016-08-29 19:12:47 +0100

    Preparing for 2.1.0

--- manifests/install.pp
+++ manifests/install.pp
@@ -32,19 +32,58 @@ class bitcoind::install {
   exec { 'puppet_uninstall_bitcoind':
     command     => 'puppet resource service bitcoind ensure=stopped;puppet resource package bitcoind ensure=absent',
     path        => ['/bin/','/sbin/','/usr/bin/','/usr/sbin/','/usr/local/bin/','/opt/puppetlabs/bin/'],
+    notify      => Exec['puppet_bitcoind_clean'],
     refreshonly => true,
-    before      => Exec['apt_update'],
   }
 
-  package { 'bitcoind':
-    ensure  => present,
-    require => Exec['apt_update'],
+  exec { 'puppet_bitcoind_clean':
+    command     => '/bin/rm /usr/bin/bitcoin*',
+
+    refreshonly => true,
   }
 
-  if $::bitcoind::install_gui == true {
-    package { 'bitcoin-qt':
+  if $::bitcoind::download_bitcoind_version != 'not_set' {
+
+    ensure_package(['wget','gzip'], { ensure => present })
+
+    $filename = "bitcoin-${::bitcoind::download_bitcoind_version}-${::bitcoind::download_bitcoind_arch}-linux-gnu.tar.gz"
+    $url      = "https://bitcoin.org/bin/bitcoin-core-${::bitcoind::download_bitcoind_version}/${filename}"
+
+    exec { 'download_bitcoind':
+      command => "/usr/bin/wget -qO /tmp/${filename} ${url}",
+      creates => "/tmp/${filename}",
+      require => Package['wget'],
+      notify  => Exec['uncompress_bitcoind'],
+    }
+
+    exec { 'uncompress_bitcoind':
+      command     => "/bin/tar -xf /tmp/${filename}",
+      notify      => Exec['install_bitcoind'],
+      refreshonly => true,
+    }
+
+    exec { 'install_bitcoind':
+      command     => '/bin/mv /tmp/bitcoind-${::bitcoind::download_bitcoind_version}/bin/* /usr/bin/',
+      require     => Exec['puppet_uninstall_bitcoind'],
+      refreshonly => true,
+    }
+
+  } else {
+
+    Exec['puppet_uninstall_bitcoind'] {
+      before => Exec['apt_update'],
+    }
+
+    package { 'bitcoind':
       ensure  => present,
       require => Exec['apt_update'],
+    }
+
+    if $::bitcoind::install_gui == true {
+      package { 'bitcoin-qt':
+        ensure  => present,
+        require => Exec['apt_update'],
+      }
     }
   }
 
